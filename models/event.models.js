c7793b6b44da74d7e535d74d44494eb9a36d6393
Priyanka <priyanka.ss1@outlook.com>
2024-11-07 18:32:17 +0530

    first commit

---
+++ models/event.models.js
@@ -0,0 +1,76 @@
+const mongoosee = require('mongoose');
+
+const eventSchema = new mongoosee.Schema(
+  {
+    eventTitle: {
+      type: String,
+      required: true,
+    },
+    eventType: {
+      type: String,
+      enum: ['Both', 'Online', 'Offline'],
+      required: true,
+    },
+    eventHostedBy: {
+      type: String,
+      required: true,
+    },
+    coverImageUrl: {
+      type: String,
+      required: true,
+    },
+    eventDescription: {
+      type: String,
+      required: true,
+    },
+    eventTags: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    eventStartTime: {
+      type: Date,
+      required: true,
+    },
+    eventEndTime: {
+      type: Date,
+      required: true,
+    },
+    speakers: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        designation: {
+          type: String,
+          required: true,
+        },
+        profileImgUrl: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    pricing: {
+      type: Number,
+      required: true,
+    },
+    eventVenue: {
+      type: String,
+      required: true,
+    },
+    eventAddress: {
+      type: String,
+      required: true,
+    },
+    age: String,
+    dressCode: String,
+  },
+  { timestamps: true }
+);
+
+const Event = mongoosee.model('Event', eventSchema);
+
+module.exports = Event;
